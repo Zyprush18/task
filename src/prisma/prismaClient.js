73ebf7f4f8ab2d895b306738f657242db6495f54
Zyprush18 <zyprush9@gmail.com>
2026-03-11 07:59:01 +0800

    update prisma client

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -1,11 +1,11 @@
-import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+// import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPostgresAdapter } from '@prisma/adapter-ppg';
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-});
+const connection = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPostgresAdapter(connection);
 
-const prisma = new PrismaClient({adapter});
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
