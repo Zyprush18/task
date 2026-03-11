f2f9a90daad59c6cc0169288b2bb642ccf400eae
Zyprush18 <zyprush9@gmail.com>
2026-03-11 08:18:35 +0800

    fix: prisma connection

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -1,6 +1,5 @@
 import 'dotenv/config';
-// import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './../../generated/prisma/client';
 import { PrismaPostgresAdapter } from '@prisma/adapter-ppg';
 
 const connection = `${process.env.DATABASE_URL}`;
